a5b2b1d3ce0cc7af2a87ee0697198ae848631c22
rogerdpack <rogerpack2005@gmail.com>
2010-11-22 08:50:15 -0700

    remove extra stuff I had in there accidentally

--- substruct/plugins/substruct/db/migrate/042_allow_nil_price_for_items.rb
+++
@@ -1,9 +0,0 @@
-class AllowNilPriceForItems < ActiveRecord::Migration
-  def self.up
-    change_column :items, :price, :float, :null => true
-  end
-
-  def self.down
-    change_column :items, :price, :float, :default => 0.0, :null => false
-  end
-end
--- substruct/plugins/substruct/lib/substruct/version.rb
+++
@@ -1,8 +0,0 @@
-module Substruct
-  module Version
-    MAJOR  = 1
-    MINOR  = 3
-    TINY   = 2
-    STRING = [MAJOR, MINOR, TINY].join('.').freeze
-  end
-end
