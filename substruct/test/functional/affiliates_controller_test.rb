69c23963c685bf4f458cc5acc0d5b2150901fad6
rogerdpack <rogerpack2005@gmail.com>
2010-11-23 09:00:40 -0700

    make tests runnable in 1.9.2

--- substruct/test/functional/affiliates_controller_test.rb
+++ substruct/test/functional/affiliates_controller_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class AffiliatesControllerTest < ActionController::TestCase
   fixtures :affiliates, :order_users, :orders, :items
--- substruct/test/unit/affiliate_payment_test.rb
+++ substruct/test/unit/affiliate_payment_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class AffiliatePaymentTest < ActiveSupport::TestCase
   fixtures :affiliates, :affiliate_payments, :orders, :preferences
